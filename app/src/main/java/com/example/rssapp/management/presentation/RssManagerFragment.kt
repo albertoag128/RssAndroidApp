527404a37ea6b8e2780d41c68a4a9b77ec7d6d7f
albertoag128 <91797204+albertoag128@users.noreply.github.com>
2022-11-30 16:31:59 +0100

    Optimization and cleaning

--- app/src/main/java/com/example/rssapp/management/presentation/RssManagerFragment.kt
+++ app/src/main/java/com/example/rssapp/management/presentation/RssManagerFragment.kt
@@ -1,40 +1,24 @@
 package com.example.rssapp.management.presentation
 
 import android.content.Context
-import android.content.SharedPreferences
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
-import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
-import android.widget.LinearLayout
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.FragmentManager
-import androidx.fragment.app.findFragment
 import androidx.lifecycle.Observer
-import androidx.navigation.findNavController
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.app.serializer.GsonSerializer
 import com.example.app.snackbar.showSnackbar
 import com.example.rssapp.R
-import com.example.rssapp.databinding.ActivityMainBinding
 import com.example.rssapp.databinding.FragmentRssManagerBinding
-import com.example.rssapp.management.data.xml.XmlLocalDataSource
 import com.example.rssapp.management.presentation.adapter.RssManagerFeedAdapter
-//import com.faltenreich.skeletonlayout.Skeleton
-//import com.faltenreich.skeletonlayout.applySkeleton
-import com.google.android.material.bottomnavigation.BottomNavigationView
-import com.google.android.material.bottomsheet.BottomSheetDialog
-import com.google.android.material.snackbar.BaseTransientBottomBar
-import com.google.android.material.snackbar.Snackbar
-import com.google.gson.Gson
 
 class RssManagerFragment : Fragment() {
-    var binding: FragmentRssManagerBinding? = null
-    var rssAdapter = RssManagerFeedAdapter()
-    val viewModel by lazy {
+    private var binding: FragmentRssManagerBinding? = null
+    private var rssAdapter = RssManagerFeedAdapter()
+    private val viewModel by lazy {
         this.activity?.let {
             RssManagerFactory().getRss(
                 GsonSerializer(), it.getPreferences(Context.MODE_PRIVATE)
@@ -59,7 +43,7 @@ class RssManagerFragment : Fragment() {
         viewModel?.getRss()
     }
 
-    fun openBottomSheet() {
+    private fun openBottomSheet() {
         binding?.rssManagerToolbar?.apply {
             title = getString(R.string.rss_manager_fragment_title)
             setOnMenuItemClickListener {
@@ -71,11 +55,11 @@ class RssManagerFragment : Fragment() {
         }
     }
 
-    fun navigateToBottomSheet() {
+    private fun navigateToBottomSheet() {
         findNavController().navigate(RssManagerFragmentDirections.actionToBottomSheet())
     }
 
-    fun setupView() {
+    private fun setupView() {
         binding?.apply {
             feedListRecyclerView.adapter = rssAdapter
             feedListRecyclerView.layoutManager = LinearLayoutManager(
@@ -91,7 +75,7 @@ class RssManagerFragment : Fragment() {
         }
     }
 
-    fun setupObservers() {
+    private fun setupObservers() {
         val rssFeedSubscriber =
             Observer<RssManagerViewModel.RssManagerFeedUiState> { uiState ->
                 rssAdapter?.setDataItems(uiState.rssFeed)
